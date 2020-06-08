fcef2e3d3f6336a5f3d8f040e64860ac147855b9
Rupinder <rupindermonga@gmail.com>
2020-06-09 00:25:35 +0530

    Ruby Practice

---
+++ test9.rb
@@ -0,0 +1,12 @@
+#Hashes - like pairs, unique keys
+states = {
+    :Pennsylvania => "PA",
+    "New York" => "NY",
+    "Oregon" => "OR"
+}
+
+puts states #Print everything
+
+#Specific value
+puts states["Oregon"]
+puts states[:Pennsylvania]
